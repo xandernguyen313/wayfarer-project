f140ac9151df9d167ce3dd0304e4699e84e05908
Sheree <sheree.edmund@gmail.com>
2022-01-21 12:11:17 -0500

    added json file information

---
+++ src/app/cities.ts
@@ -0,0 +1,143 @@
+export const CITIES = [
+ {
+    london: [
+      {
+        index: 1,
+        title: 'Big Ben',
+        description: 'This was a great iconic place in London , it was very enjoyable and nice to see if you are traveling to london, though it may not be a must see as it is UNDER CONSTRUCTION!!! We can only see the building covered which is unpleasant!',
+        img: 'https://unsplash.com/photos/mOEqOtmuPG8'
+      },
+      {
+        index: 2,
+        title: 'Buckingham Palace',
+        description: "I've been here more times than I can count but it is always nice to walk past and take a glance. There is not much to see but it is always busy with tourists, as it's a highly rated place to see in London",
+        img: "https://unsplash.com/photos/QIz5hVkNUqE"
+      },
+      {
+        index: 3,
+        title: 'London Eye',
+        description: "My wife, myself and two long-time friends, all in our sixties, had booked a flight on the London Eye for first thing in the morning. We hadn't been on the Eye for a good few years and it was definitely noticeable how very much improved everything was about the admin. When we arrived there was a queue, but this went down very quickly mainly due to the efficiency of the staff and the use of tickets on mobiles. We were soon on the Eye and had a fantastic ride due to the absolutely cloudless blue sky day. We've never seen London look so splendid and this made the ride so much more enjoyable.",
+        img: "https://unsplash.com/photos/7A15094rpp0"
+      },
+      {
+        index: 4,
+        title: "St. Paul's Cathedral",
+        description: 'Absolutely beautiful cathedral. Stunning views of London from the Stone and Golden Galleries. BUT please note this is a strenuous climb on many narrow metal spiral staircases to reach each floor which is also time consuming. Allocate at least an hour just for this part of the visit.',
+        img: "https://unsplash.com/photos/Z_zLTvMiSZ4"
+      },
+    
+    ]
+ },
+ 
+{
+  sydney: [
+    {
+      index: 1,
+      title: 'Sydney Opera House',
+      description: "We really enjoyed the tour and the guide was very informative. The tour was very Covid safe and participants were well spaced apart. We didn't go backstage at all, possibly because they were setting up shows, but we did see lot's of different parts of the Opera house. Be warned, lots of stairs.",
+      img: 'https://unsplash.com/photos/MbqMtpdzpp4'
+    },
+    {
+      index: 2,
+      title: 'Luna Park Sydney',
+      description: "Had a blast at Luna park, kids absolutely loved going on all rides in the child greenfly section of the park. We went on Boxing Day and it was definitely a good day to go, not too busy, no long waiting times for rides. Really enjoyed our day. Plenty of fun exciting things for all ages mainly kids over 85cm. We would definitely be back!",
+      img: "https://unsplash.com/photos/1GQ8mmT_mxs"
+    },
+    {
+      index: 3,
+      title: 'Sydney Harbour National Park',
+      description: 'We walked around, mostly on the cliffs, but also in other areas as well. This is one of the most spectacular parts of Sydney. The views are absolutely breathtaking. The walk can be a bit challenging if you are not physically fit, but you can take it easy and you should find it quite rewarding.',
+      img: "https://unsplash.com/photos/ZsH1wHv2iTU"
+    },
+    {
+      index: 4,
+      title: 'Royal Botanic Garden and the Domain',
+      description: "We went to the Christmas at Kew event and can't recommend it more! Magical, ethereal and breathtaking experience.. a MUST visit event for sure",
+      img: "https://unsplash.com/photos/xF9zit8T80A"
+    },
+  ]
+ },
+
+
+{
+  "san francisco": [
+    {
+      index: 1,
+      title: 'Golden Gate Bridge',
+      description: 'Such a lovely bridge and scenic area, plenty of pull over areas to soak in the views or take photos or cell phone selfies. A classic tourist place that is pretty busy but that you really want to check out.',
+      img: 'https://unsplash.com/photos/Ji_G7Bu1MoM'
+    },
+    {
+      index: 2,
+      title: 'Alcatraz Island',
+      description: "Park rangers conduct tours by recounting the prison's thrilling history along with intriguing anecdotes about Al Capone and other legendary figures that made a 'home' here.",
+      img: "https://unsplash.com/photos/cjn9CpnXn30"
+    },
+    {
+      index: 3,
+      title: "Fisherman's Wharf",
+      description: "Historic Fisherman's Wharf is the classic San Francisco experience where you will find a range of hotels, specialty restaurants, famous attractions, unique shops, breathtaking scenery and entertainment for the whole family.",
+      img: "https://unsplash.com/photos/BA9xYupJ9d4"
+    },
+    {
+      index: 4,
+      title: 'Twin Peaks',
+      description: 'A twenty-minute ride from downtown, this is the best place to catch a San Francisco sunrise.',
+      img: "https://unsplash.com/photos/q7gc6Y4mhW0"
+    },
+  ]
+ },
+
+ {
+  seattle: [
+    {
+      index: 1,
+      title: 'Space Needle',
+      description: "Seattle's “must see” is now a “must do” following the Space Needle's multimillion-dollar renovation. Glide onto The Loupe, the world's first and only rotating glass floor, for never-before-seen views of the structure and the city. Lean into tilting glass walls on the open-air deck and float over Seattle on one of 24 angled Skyriser glass benches. Sip and sample the Pacific Northwest's best while savoring the thrilling views. The future. Now open.",
+      img: 'https://unsplash.com/photos/GV8eF1jJpSs'
+    },
+    {
+      index: 2,
+      title: 'Seattle Center Monorail',
+      description: "The Monorail travels directly between Westlake Center (5th and Pine Street) and Seattle Center (adjacent to Space Needle and the Museum of Popular Culture). It is America's first full-scale commercial monorail system. The Monorail departs approximately every 10 minutes. Ticket fares are as follows: Adults: $3.00/one-way, Youth (ages 6-18): $1.50/one-way, Reduced Rate (Seniors 65+, Disabled, Persons with Medicare Card, Active Duty U.S. Military Personnel): $1.50/one-way. Roundtrip is twice the price of a one-way. Children 5 & under ride FREE! Now accepting ORCA and credit cards.",
+      img: "https://unsplash.com/photos/uh-bkQ95GB8"
+    },
+    {
+      index: 3,
+      title: 'Museum of Pop Culture (MoPOP)',
+      description: "Visit a museum like no other on earth.MoPOP, located next to Seattle's historic Space Needle, houses some of the world's most legendary pop culture artifacts. Test your DJ skills in Sound Lab, and get the scoop on the world's most mesmerizing producers of pop culture. Hands-on experiences, iconic artifacts, and award-winning exhibitions featuring luminaries in the fields of music, literature, television, and film make MoPOP a destination unlike any other.",
+      img: "https://unsplash.com/photos/7mH9jwVVwSg"
+    },
+    {
+      index: 4,
+      title: 'Pike Place Market',
+      description: "One of the few authentic farmer's markets in the United States, this hub of delicious scents, delectable eats and delightful personalities attracts nine million visitors a year.",
+      img: "https://unsplash.com/photos/xsqF178XAhk"
+    },
+  ]
+},
+
+{
+  gibraltar: [
+    {
+        index: 1,
+        title: 'Head to Europa Point',
+        description: "There are great views on a clear day across to North Africa. We saw this as part of a tour which is better so you can understand the history. You need to visit this spot if you are there.",
+        img: 'https://unsplash.com/photos/dVoNU7j3o_0'
+    },
+    {
+        index: 2,
+        title: 'Apes of Gibraltar',
+        description: "A great place to visit. Wonderful to see 3 baby apes too ! Apes are healthy and happy if they are having babies. Their mannerisms are brilliant to watch.",
+        img: "https://unsplash.com/photos/MJPQmY0__CY"
+    },
+    {
+        index: 3,
+        title: "St. Michael's Cave",
+        description: "This cave is definitely worth seeing if you are in Gibraltar. The formations are interesting and the lights make it fun. The walkways make it easy to see.It would be interesting to attend a concert there.",
+        img: "https://unsplash.com/photos/ME5QQX3gAz8"
+    },
+  ]
+},
+
+]
