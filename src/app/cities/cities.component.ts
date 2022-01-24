eae6bc6752132b819c1d7d8c526e45f9fdca93b5
Sheree <sheree.edmund@gmail.com>
2022-01-24 17:35:05 -0500

    weather for cities functional, and css

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,7 +1,7 @@
-import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
 import { CITIES } from '../cities';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -13,8 +13,9 @@ export class CitiesComponent implements OnInit {
   city: any;
   cityIndex: string = '';
   searchText: string = '';
+  weather: any;
 // let's make the URL routes available to class 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -24,12 +25,12 @@ export class CitiesComponent implements OnInit {
   
         return city.id === parseInt(this.cityIndex);
         
-
+    
 
       })
-
+      this.getWeatherDetails()
     })
-  
+ 
   
   }
 
@@ -42,5 +43,15 @@ export class CitiesComponent implements OnInit {
   redirectToPost(currentPost: any){
     this.router.navigate(['post'], {state: {currentPost: currentPost}});
   }
-}
+ getWeatherDetails() {
+    console.log(this.city.name)
+   this.http
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city.name}&appid=36567fd0102b929dc578b574dbc63eeb&&units=imperial`)
+    .subscribe(response => {
+      this.weather = response
+    }) 
+
+  }
+  }
+
 
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { WeatherSearchService } from './weathersearch.service';
+
 
 
 @Component({
@@ -10,31 +8,22 @@ import { WeatherSearchService } from './weathersearch.service';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  zip: string = '';
+  q: string = '';
   weather: any;
-  searchSubject = new Subject();
-  constructor(private weatherSearchService: WeatherSearchService) { }
+  // searchSubject = new Subject();
+  constructor() { }
 
   ngOnInit(): void {
-  this.searchSubject
-    .pipe(debounceTime(1000), distinctUntilChanged())
+  // this.searchSubject
+  //   .pipe(debounceTime(1000), distinctUntilChanged())
     
-    .subscribe(zip => {
-      console.log(this.zip);
+  //   .subscribe(zip => {
+  //     console.log(this.zip);
 
-      this.weatherSearchService.createAPIObservable(zip).subscribe(response => {
-        console.log(response);
+      // this.weather.createAPIObservable(q).subscribe(response => {
+      //   console.log(response);
 
-        this.weather = response;
-      });
-  });
-}
+      //   this.weather = response;
 
-  findWeather(zip: string): void{
-    this.searchSubject.next(zip);
-
-
+      }
   }
-
-
-}
